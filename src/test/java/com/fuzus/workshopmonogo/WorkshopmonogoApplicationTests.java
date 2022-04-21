07ab112810a14a2d22fda28abf295411713d5e42
Fuzus <matheusdamata2000@gmail.com>
2022-04-20 21:41:08 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/fuzus/workshopmonogo/WorkshopmonogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuzus.workshopmonogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmonogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
