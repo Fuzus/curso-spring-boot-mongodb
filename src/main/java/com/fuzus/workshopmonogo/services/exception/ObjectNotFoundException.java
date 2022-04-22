4f96e887fbbd923c3edf0cbc900a118525a1f890
Fuzus <matheusdamata2000@gmail.com>
2022-04-21 22:28:54 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/fuzus/workshopmonogo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fuzus.workshopmonogo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
