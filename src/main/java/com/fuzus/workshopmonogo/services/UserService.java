ea92d85bd334755945cd7463d0f25674ce48cd9c
Fuzus <matheusdamata2000@gmail.com>
2022-04-21 22:46:17 -0300

    insercao de usuario com metodo post

--- src/main/java/com/fuzus/workshopmonogo/services/UserService.java
+++ src/main/java/com/fuzus/workshopmonogo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fuzus.workshopmonogo.services;
 
 import com.fuzus.workshopmonogo.domain.User;
+import com.fuzus.workshopmonogo.dto.UserDTO;
 import com.fuzus.workshopmonogo.repository.UserRepository;
 import com.fuzus.workshopmonogo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
