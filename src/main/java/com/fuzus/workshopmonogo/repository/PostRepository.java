4a3040997b057840441c7dca158f952b545b0398
Fuzus <matheusdamata2000@gmail.com>
2022-04-25 18:04:38 -0300

    Consulta Simples com query methods

--- src/main/java/com/fuzus/workshopmonogo/repository/PostRepository.java
+++ src/main/java/com/fuzus/workshopmonogo/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.fuzus.workshopmonogo.repository;
 
 import com.fuzus.workshopmonogo.domain.Post;
-import com.fuzus.workshopmonogo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    
+
+    //query method para automatico do spring para encontrar posts que contenham texto no titulo
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
