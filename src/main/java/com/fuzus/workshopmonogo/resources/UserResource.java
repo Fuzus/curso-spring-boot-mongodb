7f1b6a64d9106b4fbb905c24d44990c19ff21631
Fuzus <matheusdamata2000@gmail.com>
2022-04-25 17:14:52 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/fuzus/workshopmonogo/resources/UserResource.java
+++ src/main/java/com/fuzus/workshopmonogo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.fuzus.workshopmonogo.resources;
 
+import com.fuzus.workshopmonogo.domain.Post;
 import com.fuzus.workshopmonogo.domain.User;
 import com.fuzus.workshopmonogo.dto.UserDTO;
 import com.fuzus.workshopmonogo.services.UserService;
@@ -50,11 +51,18 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDTO) {
         User obj = UserDTO.fromDTO(objDTO);
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }
+
 }
