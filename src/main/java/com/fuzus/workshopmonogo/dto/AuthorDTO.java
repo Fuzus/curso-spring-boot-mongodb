971a6fa002b2fec31d866718517f4a6e86039f03
Fuzus <matheusdamata2000@gmail.com>
2022-04-25 16:40:09 -0300

    Projecao dos dados do Author com DTO

---
+++ src/main/java/com/fuzus/workshopmonogo/dto/AuthorDTO.java
@@ -0,0 +1,35 @@
+package com.fuzus.workshopmonogo.dto;
+
+import com.fuzus.workshopmonogo.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {}
+
+    public AuthorDTO(User obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
